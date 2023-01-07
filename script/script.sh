7763cbb2180445e5f34d28484609faddd6b640f5
Henry Pontzer <henry@jpontzer.net>
2023-01-06 18:41:38 -0700

    Added CIS stuff

--- script/script.sh
+++ script/script.sh
@@ -79,3 +79,38 @@ overwrite "${YES} Updated cache of available packages"
 echo -e "[ i ] Upgrading installed packages..."
 dnf upgrade -y > /dev/null
 overwrite "${YES} Updated installed packages"
+
+echo -e "[ i ] Enabling firewall..."
+firewall-cmd --set-default-zone=public > /dev/null
+firewall-cmd --reload > /dev/null
+overwrite "${YES} Enabled firewall"
+
+echo -e "[ i ] Ensuring mounting of cramfs is disabled..."
+printf "install cramfs /bin/false
+blacklist cramfs
+" >> /etc/modprobe.d/cramfs.conf
+modprobe -r cramfs
+overwrite "${YES} Ensured mounting of cramfs is disabled"
+
+echo -e "[ i ] Ensuring mounting of squashfs is disabled..."
+printf "install squashfs /bin/false
+blacklist squashfs
+" >> /etc/modprobe.d/squashfs.conf
+modprobe -r squashfs
+overwrite "${YES} Ensured mounting of squashfs is disabled"
+
+echo -e "[ i ] Ensuring mounting of udf is disabled..."
+printf "install udf /bin/false
+blacklist udf
+" >> /etc/modprobe.d/udf.conf
+modprobe -r udf
+overwrite "${YES} Ensured mounting of udf is disabled"
+
+echo -e "[ i ] Disabling automounting..."
+dnf remove -y autofs &> /dev/null
+overwrite "${YES} Disabled automounting"
+
+echo -e "[ i ] Disabling USB storage..."
+dnf remove -y usbutils &> /dev/null
+overwrite "${YES} Disabled USB storage"
+
