81ff54868ca565d3bd0cc690bff48750b9bf79de
Henry Pontzer <henry@jpontzer.net>
2023-01-07 16:55:05 -0700

    CIS stuff

--- checklist.md
+++ checklist.md
@@ -1,22 +1,30 @@
 # Checklist
 
 ## 1. Forensic Questions
+
 - Solve the forensic questions. These may rely on files or programs you will eventually delete, which is why we do them first.
 
 ## 2. Updates
+
 - Open the Software app and download all available updates.
+
 ### Automatic updates
+
 - Run `sudo dnf install dnf-automatic` to install the automatic updater
 - Run `env EDITOR='gedit -w' sudoedit /etc/dnf/automatic.conf` and change `apply_updates` to `yes` so that updates are automatically installed after being downloaded
 - Run `systemctl enable --now dnf-automatic.timer` to enable the automatic update process
 
 ## 3. Services
+
 - Run `systemctl list-units --type=service` to list all active system services
 
 ## 4. Check Ports
+
 - Run `sudo lsof -i -P -n | grep -v "(ESTABLISHED)"`
+- Review the output to ensure that all services listed are required on the system. If a listed service is not required, remove the package containing the service. If the package containing the service is required, stop and mask the service
 
 ## 5. Rootkits and malware
+
 - Run `sudo dnf install rkhunter` to install Rootkit Hunter
 - Run `sudo rkhunter --update` to ensure Rootkit Hunter has the latest information
 - Run Rootkit Hunter with `sudo rkhunter -c`
--- script/script.sh
+++ script/script.sh
@@ -114,3 +114,169 @@ echo -e "[ i ] Disabling USB storage..."
 dnf remove -y usbutils &> /dev/null
 overwrite "${YES} Disabled USB storage"
 
+echo -e "[ i ] Updating core dump configuration..."
+cp coredump.conf /etc/systemd/coredump.conf
+overwrite "${YES} Updated core dump configuration"
+
+echo -e "[ i ] Ensuring address space layout randomization (ASLR) is enabled..."
+echo 2 > /proc/sys/kernel/randomize_va_space
+overwrite "${YES} Ensured address space layout randomization (ASLR) is enabled"
+
+echo -e "[ i ] Ensuring message of the day (MOTD) is configured..."
+cp motd /etc/motd
+overwrite "${YES} Ensured message of the day (MOTD) is configured"
+
+echo -e "[ i ] Ensuring local login warning banner is configured..."
+cp issue /etc/issue
+overwrite "${YES} Ensured local login warning banner is configured"
+
+echo -e "[ i ] Ensuring remote login warning banner is configured..."
+cp issue.net /etc/issue.net
+overwrite "${YES} Ensured remote login warning banner is configured"
+
+echo -e "[ i ] Ensuring permissions on /etc/motd are configured..."
+chown root:root /etc/motd
+chmod u-x,go-wx /etc/motd
+overwrite "${YES} Ensured permissions on /etc/motd are configured"
+
+echo -e "[ i ] Ensuring permissions on /etc/issue are configured..."
+chown root:root /etc/issue
+chmod u-x,go-wx /etc/issue
+overwrite "${YES} Ensured permissions on /etc/issue are configured"
+
+echo -e "[ i ] Ensuring permissions on /etc/issue.net are configured..."
+chown root:root /etc/issue.net
+chmod u-x,go-wx /etc/issue.net
+overwrite "${YES} Ensured permissions on /etc/issue.net are configured"
+
+echo -e "[ i ] Ensuring GDM login banner is configured..."
+cp gdm /etc/dconf/profile/gdm
+cp 01-banner-message /etc/dconf/db/gdm.d/01-banner-message
+dconf update
+overwrite "${YES} Ensured GDM login banner is configured"
+
+echo -e "[ i ] Ensuring last logged in user display is disabled..."
+cp 00-login-screen /etc/dconf/db/gdm.d/00-login-screen
+dconf update
+overwrite "${YES} Ensured last logged in user display is disabled"
+
+echo -e "[ i ] Ensuring XDMCP is not enabled..."
+cp custom.conf /etc/gdm/custom.conf
+overwrite "${YES} Ensured XDMCP is not enabled"
+
+echo -e "[ i ] Ensuring automatic mounting of removable media is disabled..."
+cp 00-media-automount /etc/dconf/db/local.d/00-media-automount
+dconf update
+overwrite "${YES} Ensured automatic mounting of removable media is disabled"
+
+echo -e "[ i ] Ensuring system-wide crypto policy is not legacy..."
+update-crypto-policies --set DEFAULT > /dev/null
+update-crypto-policies
+overwrite "${YES} Ensured system-wide crypto policy is not legacy"
+
+echo -e "[ i ] Ensuring xinetd is not installed..."
+dnf remove -y xinetd &> /dev/null
+overwrite "${YES} Ensured xinetd is not installed"
+
+echo -e "[ i ] Ensuring xorg-x11-server-common is not installed..."
+dnf remove -y xorg-x11-server-common &> /dev/null
+overwrite "${YES} Ensured xorg-x11-server-common is not installed"
+
+echo -e "[ i ] Ensuring Avahi Server is not installed..."
+systemctl stop avahi-daemon.socket avahi-daemon.service
+dnf remove -y avahi-autoipd avahi &> /dev/null
+overwrite "${YES} Ensured Avahi Server is not installed"
+
+echo -e "[ i ] Ensuring CUPS is not installed..."
+dnf remove -y cups &> /dev/null
+overwrite "${YES} Ensured CUPS is not installed"
+
+echo -e "[ i ] Ensuring DHCP Server is not installed..."
+dnf remove -y dhcp &> /dev/null
+overwrite "${YES} Ensured DHCP Server is not installed"
+
+echo -e "[ i ] Ensuring DNS Server is not installed..."
+dnf remove -y bind &> /dev/null
+overwrite "${YES} Ensured DNS Server is not installed"
+
+echo -e "[ i ] Ensuring FTP Server is not installed..."
+dnf remove -y ftp &> /dev/null
+overwrite "${YES} Ensured FTP Server is not installed"
+
+echo -e "[ i ] Ensuring VSFTP Server is not installed..."
+dnf remove -y vsftpd &> /dev/null
+overwrite "${YES} Ensured VSFTP Server is not installed"
+
+echo -e "[ i ] Ensuring TFTP Server is not installed..."
+dnf remove -y tftp-server &> /dev/null
+overwrite "${YES} Ensured TFTP Server is not installed"
+
+echo -e "[ i ] Ensuring a web server is not installed..."
+dnf remove -y httpd nginx &> /dev/null
+overwrite "${YES} Ensured a web server is not installed"
+
+echo -e "[ i ] Ensuring IMAP and POP3 server is not installed..."
+dnf remove -y dovecot cyrus-imapd &> /dev/null
+overwrite "${YES} Ensured IMAP and POP3 server is not installed"
+
+echo -e "[ i ] Ensuring Samba is not installed..."
+dnf remove -y samba &> /dev/null
+overwrite "${YES} Ensured Samba is not installed"
+
+echo -e "[ i ] Ensuring HTTP Proxy Server is not installed..."
+dnf remove -y squid &> /dev/null
+overwrite "${YES} Ensured HTTP Proxy Server is not installed"
+
+echo -e "[ i ] Ensuring net-snmp is not installed..."
+dnf remove -y net-snmp &> /dev/null
+overwrite "${YES} Ensured net-snmp is not installed"
+
+echo -e "[ i ] Ensuring NIS Server is not installed..."
+dnf remove -y ypserv &> /dev/null
+overwrite "${YES} Ensured NIS Server is not installed"
+
+echo -e "[ i ] Ensuring telnet-server is not installed..."
+dnf remove -y telnet-server &> /dev/null
+overwrite "${YES} Ensured telnet-server is not installed"
+
+echo -e "[ i ] Ensuring mail transfer agent is configured for local-only mode..."
+cp main.cf /etc/postfix/main.cf
+systemctl restart postfix
+overwrite "${YES} Ensured mail transfer agent is configured for local-only mode"
+
+echo -e "[ i ] Ensuring nfs-utils is not installed or the nfs-server service is masked..."
+dnf remove -y nfs-utils &> /dev/null
+overwrite "${YES} Ensured nfs-utils is not installed or the nfs-server service is masked"
+
+echo -e "[ i ] Ensuring rpcbind is not installed or the rpcbind service is masked..."
+dnf remove -y rpcbind &> /dev/null
+overwrite "${YES} Ensured rpcbind is not installed or the rpcbind service is masked"
+
+echo -e "[ i ] Ensuring rsync is not installed or the rsyncd service is masked..."
+dnf remove -y rsync &> /dev/null
+overwrite "${YES} Ensured rsync is not installed or the rsyncd service is masked"
+
+echo -e "[ i ] Ensuring NIS Client is not installed..."
+dnf remove -y ypbind &> /dev/null
+overwrite "${YES} Ensured NIS Client is not installed"
+
+echo -e "[ i ] Ensuring rsh client is not installed..."
+dnf remove -y rsh &> /dev/null
+overwrite "${YES} Ensured rsh client is not installed"
+
+echo -e "[ i ] Ensuring talk client is not installed..."
+dnf remove -y talk &> /dev/null
+overwrite "${YES} Ensured talk client is not installed"
+
+echo -e "[ i ] Ensuring telnet client is not installed..."
+dnf remove -y telnet &> /dev/null
+overwrite "${YES} Ensured telnet client is not installed"
+
+echo -e "[ i ] Ensuring LDAP client is not installed..."
+dnf remove -y openldap-clients &> /dev/null
+overwrite "${YES} Ensured LDAP client is not installed"
+
+echo -e "[ i ] Ensuring TFTP client is not installed..."
+dnf remove -y tftp &> /dev/null
+overwrite "${YES} Ensured TFTP client is not installed"
+
